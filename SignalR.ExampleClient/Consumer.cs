21aa2509e90f16a904d4c5121df4642e69774d25
Mateusz Piasecki <xxbme@eg.dk>
2023-04-21 09:33:23 +0200

    DIS SignalR example moved from DIS repo

---
+++ SignalR.ExampleClient/Consumer.cs
@@ -0,0 +1,78 @@
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace SignalR.ExampleClient
+{
+    public sealed class Consumer
+    {
+        private HubConnection _hubConnection;
+        private MemoryCache _cache;
+        private readonly string _cacheKey = "TOKEN_CACHE_KEY";
+        private static HttpClient _httpClient = new HttpClient();
+        public Consumer()
+        {
+            _cache = new MemoryCache(new MemoryCacheOptions { ExpirationScanFrequency = TimeSpan.FromMinutes(3) });
+            _hubConnection = new HubConnectionBuilder()
+                .WithUrl(new Uri($"https://dev-dis.egki.dk/caseHub"), o =>
+                {
+                    o.AccessTokenProvider = async () =>
+                    {
+                        return await _cache.GetOrCreateAsync(_cacheKey, async (cacheEntry) =>
+                        {
+                            if (cacheEntry != null && cacheEntry?.Value != null)
+                                return cacheEntry.Value!.ToString()!;
+
+                            var response = await _httpClient.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+                            {
+                                ClientId = "CLIENT_ID",
+                                ClientSecret = "CLIENT_SECRET",
+                                Address = "https://dev-sts.egki.dk/auth/realms/egki/protocol/openid-connect/token",
+                            }, CancellationToken.None);
+
+                            cacheEntry!.SetValue(response.AccessToken);
+
+                            return response.AccessToken;
+                        });
+
+                        
+                    };
+                })
+                .WithAutomaticReconnect()
+                .Build();
+
+            _hubConnection.On<string>(
+                "CaseCreatedNotificationAsync", OnCaseCreatedNotificationReceivedAsync);
+
+            _hubConnection.On<string>(
+                "CaseUpdatedNotificationAsync", OnCaseUpdatedNotificationReceivedAsync);
+
+            _hubConnection.On<string>(
+                "CaseDeletedNotificationAsync", OnCaseDeletedNotificationReceivedAsync);
+        }
+
+        public async Task StartNotificationConnectionAsync()
+        {
+            await _hubConnection.StartAsync();
+
+            Console.WriteLine("Connected to SignalR hub");
+        }
+
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        private async Task OnCaseCreatedNotificationReceivedAsync(string caseId)
+        {
+            Console.WriteLine($"Created: {caseId}");
+        }
+
+        private async Task OnCaseUpdatedNotificationReceivedAsync(string caseId)
+        {
+            Console.WriteLine($"Updated: {caseId}");
+        }
+
+        private async Task OnCaseDeletedNotificationReceivedAsync(string caseId)
+        {
+            Console.WriteLine($"Deleted: {caseId}");
+        }
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+    }
+}
---
+++ SignalR.ExampleClient/Program.cs
@@ -0,0 +1,10 @@
+﻿using SignalR.ExampleClient;
+
+//Example app, not production ready solution
+
+Console.WriteLine("App starting!");
+
+var consumer = new Consumer();
+await consumer.StartNotificationConnectionAsync();
+
+Console.ReadKey();
